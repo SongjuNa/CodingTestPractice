c33cc9ad3617d1e7d32fd84b43d5c43db8f3202a
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2025-08-03 23:47:27 +0900

    [level 1] Title: 과일 장수, Time: 174.62 ms, Memory: 28.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -0,0 +1,9 @@
+def solution(k, m, score):
+    score = sorted(score)
+    answer = 0
+
+    for i in range(len(score)//m):
+        for j in range(m-1):
+            score.pop()
+        answer += score.pop() * m
+    return answer
