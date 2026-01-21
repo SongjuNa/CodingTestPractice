d71d42c56bd8a79029ad132c352d033220c55103
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-01-21 14:26:31 +0900

    [Bronze III] Title: 오븐 시계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+c = int(input())
+
+total = b + c
+
+if total < 60:
+    print(a, total)
+else:
+    a = (a + total // 60) % 24
+    b = total % 60
+    print(a, b)
