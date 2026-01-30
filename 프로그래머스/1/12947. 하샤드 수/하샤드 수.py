e8fc9483d9a148f4936d2681d22be5bd1650e632
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-01-30 20:08:01 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -1,11 +1,13 @@
 def solution(x):
     x = str(x)
-    total = 0
     
-    for i in range(len(x)):
-        total += int(x[i])
+    total = 0
+    for num in x:
+        total += int(num)
+    
     if int(x) % total == 0:
         answer = True
     else:
         answer = False
+        
     return answer
