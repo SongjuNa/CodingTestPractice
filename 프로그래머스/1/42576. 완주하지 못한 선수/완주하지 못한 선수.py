8d91bd2525b8d5abd203b63f4f4fcd80891aea08
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-01-30 20:33:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 48.79 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,14 +1,12 @@
-# 해시 기초 문제!
-
 def solution(participant, completion):
-    count = {} # 딕셔너리
-
+    incompletion = {}
+    
     for p in participant:
-        count[p] = count.get(p, 0) + 1  # count에서 p 있으면 가져오고, 없으면 0(그리고 1(명)) 추가)
-
-    for c in completion: # count에서 완주자 삭제하기
-        count[c] -= 1
-
-    for name in count:
-        if count[name] == 1:
+        incompletion[p] = incompletion.get(p, 0) +1
+    for c in completion:
+        incompletion[c] -= 1
+    
+    for name in incompletion:
+        if incompletion[name] == 1:
             return name
+    
