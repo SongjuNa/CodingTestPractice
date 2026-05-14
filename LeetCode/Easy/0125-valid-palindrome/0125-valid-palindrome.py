ffe641f55b6a2ec74cfbd1941ff7fc51ac306f8f
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-05-14 15:10:16 +0900

    Time: 5 ms (87.09%), Space: 20.3 MB (30.55%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,10 +2,8 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         rmv_non_alphanum = "".join(char for char in s.lower() if char.isalnum())
         
-        # 이제 깨끗해진 clean_s를 뒤집습니다.
         answer = rmv_non_alphanum[::-1]
         
-        # clean_s와 뒤집은 answer를 비교합니다.
         if rmv_non_alphanum == answer:
             return True
         else:
