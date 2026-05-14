2ecec1eaa82df0b48bd13c85aebbcd8b92a8d07e
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-05-14 15:48:45 +0900

    Time: 0 ms (100%), Space: 19.4 MB (5.98%) - LeetHub

--- LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
+++ LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,8 +4,8 @@ class Solution:
         stack = []
         
         for i in s:
-            if i in mapping:
-                top = stack.pop() if stack else '#'
+            if i in mapping:  # i가 키인지(닫는 괄호인지)
+                top = stack.pop() if stack else '#'  # 스택에 값이 있으면 가장 최근 값 꺼내고, 스택이 비었다면 더미 값 저장
                 if mapping[i] != top:
                     return False
             else:
