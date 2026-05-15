6224a1085b3af2a969cab02b774e1a622ec10b01
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-05-15 14:37:48 +0900

    Time: 10 ms (29.11%), Space: 22.5 MB (70.28%) - LeetHub

--- LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.py
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.py
@@ -4,11 +4,11 @@ class Solution:
         intervals.sort()
 
         for curr in intervals:
-            # 조건 1: 스택이 비어있거나, 안 겹칠 때 -> 그냥 스택에 넣는다
+            # 스택이 비어있거나 구간이 안 겹칠 때
             if not output or output[-1][1] < curr[0]:
                 output.append(curr)
                 
-            # 조건 2: 겹칠 때 -> 스택 마지막 구간의 '끝나는 시간'을 늘려준다!
+            # 구간 겹칠 때
             else:
                 output[-1][1] = max(output[-1][1], curr[1])
             
