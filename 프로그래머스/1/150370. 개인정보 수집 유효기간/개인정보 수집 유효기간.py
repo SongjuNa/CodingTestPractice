d8fce9472401bfbf4008930f20b6ada296090185
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-05-15 15:40:53 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.12 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,17 +1,29 @@
-def calculate(date, r) :
-    date = list(map(int, date.split('.')))
-    return (date[0] * 12 + date[1]) * 28 + date[2] + r * 28
-
-
-
 def solution(today, terms, privacies):
-    split_terms = [i.split() for i in terms]
     answer = []
-    for i in range(len(privacies)) :
-        period, priv = privacies[i].split()
-        j = 0
-        while j < len(split_terms) :
-            if split_terms[j][0] == priv : break
-            j += 1
-        if calculate(today, 0) >= calculate(period, int(split_terms[j][1])) : answer.append(i + 1)
+    
+    # 오늘 날짜를 총 일수로 변환
+    y, m, d = today.split(".")
+    today_days = (int(y) * 12 * 28) + (int(m) * 28) + int(d)
+    
+    # terms 배열을 딕셔너리로 변환(해시)
+    term_dict = {}
+    for term in terms:
+        name, month = term.split(" ")
+        term_dict[name] = int(month)
+         
+    # privacies 배열
+    for i, p in enumerate(privacies):
+        date, term_type = p.split(" ") 
+        p_y, p_m, p_d = date.split(".")
+        
+        # 개인정보 수집 일자를 총 일수로 변환
+        join_days = (int(p_y) * 12 * 28) + (int(p_m) * 28) + int(p_d)
+        
+        # 만료일 총 일수 계산
+        expire_days = join_days + (term_dict[term_type] * 28)
+        
+        # 오늘 총 일수가 만료일보다 크거나 같으면 파기
+        if today_days >= expire_days:
+            answer.append(i + 1)
+
     return answer
