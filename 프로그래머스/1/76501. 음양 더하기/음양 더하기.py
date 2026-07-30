91cbcedaf3328a1a824f22446994ae04e6602870
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-07-30 14:48:24 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,3 +1,8 @@
 def solution(absolutes, signs):
-    answer = 123456789
+    answer = 0
+    for i in range(len(signs)):
+        if signs[i]:
+            answer += absolutes[i]
+        else:
+            answer -= absolutes[i]
     return answer
