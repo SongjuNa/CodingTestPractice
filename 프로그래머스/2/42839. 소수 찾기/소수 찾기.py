a1fb4513be55ff31ec77556455135d257e2406ba
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-08-03 23:58:49 +0900

    [level 2] Title: 소수 찾기, Time: 1037.03 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,14 +1,14 @@
-from itertools import permutations
+from itertools import permutations # 순열 모듈 사용
 def solution(numbers):
     
     count = 0
-    candidates = set()  # 중복제거(후보군 만들기)
+    unique_nums = set()  # 집합으로 중복 제거
     for i in range(1, len(numbers)+1):
         for p in permutations(numbers, i):  # 소수 생성
             num = int(''.join(p))
-            candidates.add(num)
+            unique_nums.add(num)
     
-    for num in candidates:
+    for num in unique_nums:
         if num < 2:
             continue
         
