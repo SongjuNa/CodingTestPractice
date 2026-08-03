a2dd4feb80002f3c201d00c3a8a18643a613d9fc
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-08-04 00:08:20 +0900

    [level 2] Title: 소수 찾기, Time: 1014.38 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,22 +1,26 @@
-from itertools import permutations # 순열 모듈 사용
+from itertools import permutations #순열 모듈 사용
 def solution(numbers):
     
     count = 0
-    unique_nums = set()  # 집합으로 중복 제거
+    unique_nums = set()  #집합으로 중복 숫자 조합 제거
+    
+    #생성 가능한 모든 자릿수 순열
     for i in range(1, len(numbers)+1):
-        for p in permutations(numbers, i):  # 소수 생성
+        for p in permutations(numbers, i): 
             num = int(''.join(p))
             unique_nums.add(num)
     
+    #소수 판별
     for num in unique_nums:
-        if num < 2:
+        if num < 2: #0, 1은 소수 아니니까 패스
             continue
         
-        is_prime = True
+        is_sosu = True
+        #2~(num-1)까지 나누어 떨어지는 수 있는지 체크
         for i in range(2, num):
             if num % i == 0:
-                is_prime = False
+                is_sosu = False #나누어 떨어지면 소수가 아니니까 break
                 break
-        if is_prime:
+        if is_sosu:
             count += 1
     return count
