da0d352968aacc0b539426326c047dd035b1b89d
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-08-11 11:12:55 +0900

    [level 2] Title: 프로세스, Time: 0.26 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,13 +1,13 @@
 def solution(priorities, location):
-    #대기열(queue) 생성 (위치번호 부여)
+    #queue 생성 with 위치번호 부여
     queue = [(i, p) for i, p in enumerate(priorities)]
     answer = 0 
 
     while True: 
-        # 맨 앞 꺼내기
+        #맨 앞의 값 꺼내서 cur에 담으면
         cur = queue.pop(0)
         
-        # 현재 값보다 중요도 더 큰 거 있으면 queue에 추가
+        #현재 값보다 중요도 더 큰 거 있으면 queue에 추가
         if any(cur[1] < q[1] for q in queue):
             queue.append(cur)
             
