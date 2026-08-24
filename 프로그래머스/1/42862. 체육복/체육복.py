f23461f168ccd42c6c6490c6289db7050072daa5
Songju Na <102519785+SongjuNa@users.noreply.github.com>
2026-08-24 14:33:09 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,19 +1,14 @@
 def solution(n, lost, reserve):
-    # 차집합 사용
-    # 진짜 빌려줄 수 있는 학생들
-    real_reserve = set(reserve) - set(lost)
-    
-    # 체육복 아예 없는 학생들
+    #체육복 빌려줄 수 있는 학생 수(여벌 있는 학생 중 도난 당한 학생 수 반영)
+    real_reserve = set(reserve) - set(lost)    
+    #체육복 없는 학생
     real_lost = set(lost) - set(reserve)
     
-    for r in real_reserve:
-        # 앞번호 체크
-        if r - 1 in real_lost:
-            real_lost.remove(r - 1) 
+    for i in real_reserve:
+        if i-1 in real_lost:
+            real_lost.remove(i-1)
+        elif i+1 in real_lost:
+            real_lost.remove(i+1)
             
-        # 뒷번호 체크
-        elif r + 1 in real_lost:
-            real_lost.remove(r + 1) 
-            
-    # 전체 학생 - 체육수업 참여 불가한 학생
     return n - len(real_lost)
+    
